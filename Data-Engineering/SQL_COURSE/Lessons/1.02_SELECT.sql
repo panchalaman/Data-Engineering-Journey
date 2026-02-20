d5283ceae9cc34f42fbea7c7d7b2921553647f12
panchalaman <panchalaman@hotmail.com>
2026-02-20 16:42:55 +0100

    Querries with Output

--- Data-Engineering/SQL_COURSE/Lessons/1.02_SELECT.sql
+++ Data-Engineering/SQL_COURSE/Lessons/1.02_SELECT.sql
@@ -20,6 +20,25 @@ SELECT *
 FROM job_postings_fact
 LIMIT 10;
 
+/*
+┌────────┬────────────┬─────────────────────┬──────────────────────┬───────────────────┬───┬──────────────────────┬───────────────┬─────────────┬─────────────────┬─────────────────┐
+│ job_id │ company_id │   job_title_short   │      job_title       │   job_location    │ … │ job_health_insurance │  job_country  │ salary_rate │ salary_year_avg │ salary_hour_avg │
+│ int32  │   int32    │       varchar       │       varchar        │      varchar      │   │       boolean        │    varchar    │   varchar   │     double      │     double      │
+├────────┼────────────┼─────────────────────┼──────────────────────┼───────────────────┼───┼──────────────────────┼───────────────┼─────────────┼─────────────────┼─────────────────┤
+│   4593 │       4593 │ Data Analyst        │ Data Analyst         │ New York, NY      │ … │ false                │ United States │ NULL        │            NULL │            NULL │
+│   4594 │       4594 │ Data Analyst        │ Data Analyst         │ Washington, DC    │ … │ true                 │ United States │ NULL        │            NULL │            NULL │
+│   4595 │       4595 │ Data Analyst        │ Data Analyst         │ Fairfax, VA       │ … │ false                │ United States │ NULL        │            NULL │            NULL │
+│   4596 │       4596 │ Senior Data Analyst │ Senior Data Analys…  │ Worcester, MA     │ … │ true                 │ United States │ NULL        │            NULL │            NULL │
+│   4597 │       4597 │ Data Analyst        │ Data Analyst         │ Sunnyvale, CA     │ … │ false                │ United States │ NULL        │            NULL │            NULL │
+│   4598 │       4598 │ Data Analyst        │ Jr. Data Analyst     │ Torrance, CA      │ … │ false                │ United States │ NULL        │            NULL │            NULL │
+│   4599 │       4599 │ Data Analyst        │ Data Analyst         │ San Francisco, CA │ … │ false                │ United States │ NULL        │            NULL │            NULL │
+│   4600 │       4600 │ Data Analyst        │ Loyalty Data Analy…  │ Pleasanton, CA    │ … │ false                │ United States │ NULL        │            NULL │            NULL │
+│   4601 │       4601 │ Senior Data Analyst │ Senior data analyst  │ Rosemead, CA      │ … │ true                 │ United States │ NULL        │            NULL │            NULL │
+│   4602 │       4602 │ Business Analyst    │ Business Analyst -…  │ Thousand Oaks, CA │ … │ false                │ United States │ NULL        │            NULL │            NULL │
+├────────┴────────────┴─────────────────────┴──────────────────────┴───────────────────┴───┴──────────────────────┴───────────────┴─────────────┴─────────────────┴─────────────────┤
+│ 10 rows                                                                                                                                                     16 columns (10 shown) │
+└───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+*/
 -- Much better. Now let's pick specific columns instead of *.
 -- In real work, you almost never use * — you pick what you need.
 
@@ -30,7 +49,25 @@ SELECT
     salary_year_avg
 FROM job_postings_fact
 LIMIT 10;
-
+ /*
+┌────────┬───────────────────────────────────────────┬───────────────────┬─────────────────┐
+│ job_id │                 job_title                 │   job_location    │ salary_year_avg │
+│ int32  │                  varchar                  │      varchar      │     double      │
+├────────┼───────────────────────────────────────────┼───────────────────┼─────────────────┤
+│   4593 │ Data Analyst                              │ New York, NY      │            NULL │
+│   4594 │ Data Analyst                              │ Washington, DC    │            NULL │
+│   4595 │ Data Analyst                              │ Fairfax, VA       │            NULL │
+│   4596 │ Senior Data Analyst / Platform Experience │ Worcester, MA     │            NULL │
+│   4597 │ Data Analyst                              │ Sunnyvale, CA     │            NULL │
+│   4598 │ Jr. Data Analyst                          │ Torrance, CA      │            NULL │
+│   4599 │ Data Analyst                              │ San Francisco, CA │            NULL │
+│   4600 │ Loyalty Data Analyst III                  │ Pleasanton, CA    │            NULL │
+│   4601 │ Senior data analyst                       │ Rosemead, CA      │            NULL │
+│   4602 │ Business Analyst - Taxonomy/Ontology      │ Thousand Oaks, CA │            NULL │
+├────────┴───────────────────────────────────────────┴───────────────────┴─────────────────┤
+│ 10 rows                                                                        4 columns │
+└──────────────────────────────────────────────────────────────────────────────────────────┘
+ */
 
 -- ============================================================
 -- ALIASES: Renaming Columns with AS
@@ -46,6 +83,26 @@ SELECT
 FROM job_postings_fact
 LIMIT 10;
 
+/*
+
+┌────────┬───────────────────────────────────────────┬───────────────────┬────────────┐
+│ job_id │                   title                   │     location      │ avg_salary │
+│ int32  │                  varchar                  │      varchar      │   double   │
+├────────┼───────────────────────────────────────────┼───────────────────┼────────────┤
+│   4593 │ Data Analyst                              │ New York, NY      │       NULL │
+│   4594 │ Data Analyst                              │ Washington, DC    │       NULL │
+│   4595 │ Data Analyst                              │ Fairfax, VA       │       NULL │
+│   4596 │ Senior Data Analyst / Platform Experience │ Worcester, MA     │       NULL │
+│   4597 │ Data Analyst                              │ Sunnyvale, CA     │       NULL │
+│   4598 │ Jr. Data Analyst                          │ Torrance, CA      │       NULL │
+│   4599 │ Data Analyst                              │ San Francisco, CA │       NULL │
+│   4600 │ Loyalty Data Analyst III                  │ Pleasanton, CA    │       NULL │
+│   4601 │ Senior data analyst                       │ Rosemead, CA      │       NULL │
+│   4602 │ Business Analyst - Taxonomy/Ontology      │ Thousand Oaks, CA │       NULL │
+├────────┴───────────────────────────────────────────┴───────────────────┴────────────┤
+│ 10 rows                                                                   4 columns │
+└─────────────────────────────────────────────────────────────────────────────────────┘
+*/
 -- You can also skip the AS keyword — it's optional.
 -- But I always use it because it's clearer.
 
@@ -55,6 +112,19 @@ SELECT
 FROM job_postings_fact
 LIMIT 5;
 
+/*
+
+┌────────┬───────────────────────────────────────────┐
+│ job_id │                   title                   │
+│ int32  │                  varchar                  │
+├────────┼───────────────────────────────────────────┤
+│   4593 │ Data Analyst                              │
+│   4594 │ Data Analyst                              │
+│   4595 │ Data Analyst                              │
+│   4596 │ Senior Data Analyst / Platform Experience │
+│   4597 │ Data Analyst                              │
+└────────┴───────────────────────────────────────────┘
+*/
 
 -- ============================================================
 -- TABLE ALIASES
@@ -82,10 +152,61 @@ LIMIT 5;
 SELECT DISTINCT job_title_short
 FROM job_postings_fact;
 
+/*
+
+┌───────────────────────────┐
+│      job_title_short      │
+│          varchar          │
+├───────────────────────────┤
+│ Senior Data Analyst       │
+│ Machine Learning Engineer │
+│ Cloud Engineer            │
+│ Data Engineer             │
+│ Data Scientist            │
+│ Business Analyst          │
+│ Data Analyst              │
+│ Software Engineer         │
+│ Senior Data Engineer      │
+│ Senior Data Scientist     │
+├───────────────────────────┤
+│          10 rows          │
+└───────────────────────────┘
+*/
+
 -- What countries have job postings?
 SELECT DISTINCT job_country
 FROM job_postings_fact
 LIMIT 20;
+/*
+
+┌─────────────┐
+│ job_country │
+│   varchar   │
+├─────────────┤
+│ Denmark     │
+│ Hungary     │
+│ Oman        │
+│ Nicaragua   │
+│ Kuwait      │
+│ Afghanistan │
+│ Guam        │
+│ Rwanda      │
+│ Hong Kong   │
+│ Ecuador     │
+│ Argentina   │
+│ Slovakia    │
+│ Norway      │
+│ New Zealand │
+│ Moldova     │
+│ Philippines │
+│ Nigeria     │
+│ Serbia      │
+│ Cameroon    │
+│ Guinea      │
+├─────────────┤
+│   20 rows   │
+└─────────────┘
+*/
 
 -- DISTINCT on multiple columns means unique COMBINATIONS
 SELECT DISTINCT
@@ -93,7 +214,36 @@ SELECT DISTINCT
     job_country
 FROM job_postings_fact
 LIMIT 20;
+/*
 
+┌───────────────────────┬───────────────┐
+│    job_title_short    │  job_country  │
+│        varchar        │    varchar    │
+├───────────────────────┼───────────────┤
+│ Data Analyst          │ Japan         │
+│ Senior Data Scientist │ United States │
+│ Data Analyst          │ Spain         │
+│ Business Analyst      │ Denmark       │
+│ Senior Data Engineer  │ Ireland       │
+│ Senior Data Scientist │ India         │
+│ Data Analyst          │ Mexico        │
+│ Software Engineer     │ France        │
+│ Data Analyst          │ Netherlands   │
+│ Data Scientist        │ Slovakia      │
+│ Business Analyst      │ Oman          │
+│ Software Engineer     │ Canada        │
+│ Data Analyst          │ Belgium       │
+│ Software Engineer     │ Qatar         │
+│ Data Scientist        │ Argentina     │
+│ Data Analyst          │ Russia        │
+│ Senior Data Scientist │ Germany       │
+│ Data Engineer         │ Lithuania     │
+│ Data Scientist        │ New Zealand   │
+│ Software Engineer     │ China         │
+├───────────────────────┴───────────────┤
+│ 20 rows                     2 columns │
+└───────────────────────────────────────┘
+*/
 
 -- ============================================================
 -- EXPRESSIONS & CALCULATIONS
@@ -109,6 +259,26 @@ SELECT
 FROM job_postings_fact
 WHERE salary_year_avg IS NOT NULL
 LIMIT 10;
+/*
+
+┌────────┬───────────────────────────────────┬───────────────┬────────────────────┐
+│ job_id │             job_title             │ annual_salary │   monthly_salary   │
+│ int32  │              varchar              │    double     │       double       │
+├────────┼───────────────────────────────────┼───────────────┼────────────────────┤
+│   4651 │ Data Scientist                    │      110000.0 │  9166.666666666666 │
+│   4699 │ Data Engineer                     │       65000.0 │  5416.666666666667 │
+│   4804 │ Hospitality Operations Analyst    │       90000.0 │             7500.0 │
+│   4810 │ Data Analytics Professional       │       55000.0 │  4583.333333333333 │
+│   4833 │ Lead Data Scientist (Hybrid)      │      120531.0 │           10044.25 │
+│   4846 │ Data Engineer - Revenue Platforms │      300000.0 │            25000.0 │
+│   5089 │ Junior Data Analyst               │       51000.0 │             4250.0 │
+│   5123 │ Data Science Manager              │      133500.0 │            11125.0 │
+│   5321 │ HR Data Analyst                   │       77500.0 │  6458.333333333333 │
+│   5325 │ Data Scientist                    │      125000.0 │ 10416.666666666666 │
+├────────┴───────────────────────────────────┴───────────────┴────────────────────┤
+│ 10 rows                                                               4 columns │
+└─────────────────────────────────────────────────────────────────────────────────┘
+*/
 
 -- String concatenation (joining text together)
 SELECT
@@ -116,7 +286,26 @@ SELECT
     job_title_short || ' at ' || job_location AS job_summary
 FROM job_postings_fact
 LIMIT 10;
+/*
 
+┌────────┬───────────────────────────────────────┐
+│ job_id │              job_summary              │
+│ int32  │                varchar                │
+├────────┼───────────────────────────────────────┤
+│   4593 │ Data Analyst at New York, NY          │
+│   4594 │ Data Analyst at Washington, DC        │
+│   4595 │ Data Analyst at Fairfax, VA           │
+│   4596 │ Senior Data Analyst at Worcester, MA  │
+│   4597 │ Data Analyst at Sunnyvale, CA         │
+│   4598 │ Data Analyst at Torrance, CA          │
+│   4599 │ Data Analyst at San Francisco, CA     │
+│   4600 │ Data Analyst at Pleasanton, CA        │
+│   4601 │ Senior Data Analyst at Rosemead, CA   │
+│   4602 │ Business Analyst at Thousand Oaks, CA │
+├────────┴───────────────────────────────────────┤
+│ 10 rows                              2 columns │
+└────────────────────────────────────────────────┘
+*/
 
 -- ============================================================
 -- NULL VALUES
@@ -136,6 +325,27 @@ FROM job_postings_fact
 WHERE salary_year_avg IS NULL
 LIMIT 10;
 
+/*
+
+┌────────┬───────────────────────────────────────────┬─────────────────┐
+│ job_id │                 job_title                 │ salary_year_avg │
+│ int32  │                  varchar                  │     double      │
+├────────┼───────────────────────────────────────────┼─────────────────┤
+│   4593 │ Data Analyst                              │            NULL │
+│   4594 │ Data Analyst                              │            NULL │
+│   4595 │ Data Analyst                              │            NULL │
+│   4596 │ Senior Data Analyst / Platform Experience │            NULL │
+│   4597 │ Data Analyst                              │            NULL │
+│   4598 │ Jr. Data Analyst                          │            NULL │
+│   4599 │ Data Analyst                              │            NULL │
+│   4600 │ Loyalty Data Analyst III                  │            NULL │
+│   4601 │ Senior data analyst                       │            NULL │
+│   4602 │ Business Analyst - Taxonomy/Ontology      │            NULL │
+├────────┴───────────────────────────────────────────┴─────────────────┤
+│ 10 rows                                                    3 columns │
+└──────────────────────────────────────────────────────────────────────┘
+*/
+
 -- Find rows where salary EXISTS
 SELECT
     job_id,
@@ -144,6 +354,26 @@ SELECT
 FROM job_postings_fact
 WHERE salary_year_avg IS NOT NULL
 LIMIT 10;
+/*
+
+┌────────┬───────────────────────────────────┬─────────────────┐
+│ job_id │             job_title             │ salary_year_avg │
+│ int32  │              varchar              │     double      │
+├────────┼───────────────────────────────────┼─────────────────┤
+│   4651 │ Data Scientist                    │        110000.0 │
+│   4699 │ Data Engineer                     │         65000.0 │
+│   4804 │ Hospitality Operations Analyst    │         90000.0 │
+│   4810 │ Data Analytics Professional       │         55000.0 │
+│   4833 │ Lead Data Scientist (Hybrid)      │        120531.0 │
+│   4846 │ Data Engineer - Revenue Platforms │        300000.0 │
+│   5089 │ Junior Data Analyst               │         51000.0 │
+│   5123 │ Data Science Manager              │        133500.0 │
+│   5321 │ HR Data Analyst                   │         77500.0 │
+│   5325 │ Data Scientist                    │        125000.0 │
+├────────┴───────────────────────────────────┴─────────────────┤
+│ 10 rows                                            3 columns │
+└──────────────────────────────────────────────────────────────┘
+*/
 
 -- IMPORTANT: You can NOT use = NULL or != NULL
 -- These don't work:
@@ -162,3 +392,32 @@ LIMIT 10;
 --
 -- 3. Select job_id, job_title, and calculate what a 10% raise
 --    on salary_year_avg would be (only where salary exists)
+SELECT
+    job_id,
+    job_title,
+    salary_year_avg * 1.10 AS salary_with_raise
+FROM job_postings_fact
+WHERE salary_year_avg IS NOT NULL
+LIMIT 10;
+
+/*
+
+┌────────┬───────────────────────────────────┬────────────────────┐
+│ job_id │             job_title             │ salary_with_raise  │
+│ int32  │              varchar              │       double       │
+├────────┼───────────────────────────────────┼────────────────────┤
+│   4651 │ Data Scientist                    │ 121000.00000000001 │
+│   4699 │ Data Engineer                     │            71500.0 │
+│   4804 │ Hospitality Operations Analyst    │  99000.00000000001 │
+│   4810 │ Data Analytics Professional       │  60500.00000000001 │
+│   4833 │ Lead Data Scientist (Hybrid)      │           132584.1 │
+│   4846 │ Data Engineer - Revenue Platforms │           330000.0 │
+│   5089 │ Junior Data Analyst               │  56100.00000000001 │
+│   5123 │ Data Science Manager              │           146850.0 │
+│   5321 │ HR Data Analyst                   │            85250.0 │
+│   5325 │ Data Scientist                    │           137500.0 │
+├────────┴───────────────────────────────────┴────────────────────┤
+│ 10 rows                                               3 columns │
+└─────────────────────────────────────────────────────────────────┘
+*/
+--Practise these basics until they feel natural. In the next lesson, we'll start joining tables together to combine data from multiple sources.
--- Data-Engineering/SQL_COURSE/Lessons/1.03_WHERE.sql
+++ Data-Engineering/SQL_COURSE/Lessons/1.03_WHERE.sql
@@ -19,6 +19,27 @@ SELECT
 FROM job_postings_fact
 WHERE job_location = 'New York, NY'
 LIMIT 10;
+/*
+
+┌────────┬──────────────────────────────────────────────────────────────┬──────────────┐
+│ job_id │                          job_title                           │ job_location │
+│ int32  │                           varchar                            │   varchar    │
+├────────┼──────────────────────────────────────────────────────────────┼──────────────┤
+│   4593 │ Data Analyst                                                 │ New York, NY │
+│   4638 │ Marketing & Growth Data scientist                            │ New York, NY │
+│   4796 │ Analytics & Data Science Data Engineer-NYC/Bridgewater, NJ   │ New York, NY │
+│   4850 │ Data Engineer - 2568                                         │ New York, NY │
+│   4967 │ Lead Applied Data Scientist                                  │ New York, NY │
+│   4971 │ Instructor, Data Science                                     │ New York, NY │
+│   5577 │ Senior Data Scientist - Now Hiring                           │ New York, NY │
+│   5772 │ NYL Data Scientist/Statistician Internship                   │ New York, NY │
+│   5867 │ Data Scientist / Machine Learning Engineer - Now Hiring      │ New York, NY │
+│   5881 │ Software Engineer, Data Engineering, Ad Platform Engineering │ New York, NY │
+├────────┴──────────────────────────────────────────────────────────────┴──────────────┤
+│ 10 rows                                                                    3 columns │
+└──────────────────────────────────────────────────────────────────────────────────────┘
+*/
+
 
 -- Jobs with salary above 150k
 SELECT
@@ -28,6 +49,26 @@ SELECT
 FROM job_postings_fact
 WHERE salary_year_avg > 150000
 LIMIT 10;
+/*
+
+┌────────┬───────────────────────────────────────────────────────────────────┬─────────────────┐
+│ job_id │                             job_title                             │ salary_year_avg │
+│ int32  │                              varchar                              │     double      │
+├────────┼───────────────────────────────────────────────────────────────────┼─────────────────┤
+│   4846 │ Data Engineer - Revenue Platforms                                 │        300000.0 │
+│   5326 │ Staff Data Scientist                                              │        202500.0 │
+│   5330 │ Data Engineer                                                     │        165000.0 │
+│   5369 │ Data Engineer - Spark, Scala, Kafka, Flink                        │        175000.0 │
+│   5578 │ Director, Data Science                                            │        175000.0 │
+│   6351 │ Data Engineer                                                     │        165000.0 │
+│   8506 │ Sr. Marketing Data Analyst                                        │        170000.0 │
+│   8510 │ Data Scientist/Engineer - Bay Area Startup with GREAT benefits    │        210000.0 │
+│   8698 │ Sr. Manager, Data Science - Corporate Audit and Security Services │        222589.0 │
+│   8844 │ AIML - Sr Data Science Manager, AIML Data                         │        175000.0 │
+├────────┴───────────────────────────────────────────────────────────────────┴─────────────────┤
+│ 10 rows                                                                            3 columns │
+└──────────────────────────────────────────────────────────────────────────────────────────────┘
+*/
 
 -- Comparison operators:
 --   =    equal to
@@ -52,6 +93,26 @@ FROM job_postings_fact
 WHERE job_title_short = 'Data Engineer'
   AND salary_year_avg > 120000
 LIMIT 10;
+/*
+
+┌────────┬─────────────────┬─────────────────┬───────────────────┐
+│ job_id │ job_title_short │ salary_year_avg │   job_location    │
+│ int32  │     varchar     │     double      │      varchar      │
+├────────┼─────────────────┼─────────────────┼───────────────────┤
+│   4846 │ Data Engineer   │        300000.0 │ Boston, NY        │
+│   5330 │ Data Engineer   │        165000.0 │ Arlington, VA     │
+│   5369 │ Data Engineer   │        175000.0 │ San Jose, CA      │
+│   6351 │ Data Engineer   │        165000.0 │ Dearborn, MI      │
+│   8474 │ Data Engineer   │        144481.5 │ St. Louis, MO     │
+│   8720 │ Data Engineer   │        125000.0 │ Goleta, CA        │
+│   8830 │ Data Engineer   │        125000.0 │ San Francisco, CA │
+│   9580 │ Data Engineer   │        150000.0 │ Beaverton, OR     │
+│   9936 │ Data Engineer   │        123600.0 │ Goodyear, AZ      │
+│  10116 │ Data Engineer   │        123600.0 │ Glendale, AZ      │
+├────────┴─────────────────┴─────────────────┴───────────────────┤
+│ 10 rows                                              4 columns │
+└────────────────────────────────────────────────────────────────┘
+*/
 
 -- OR = at least one condition must be true
 SELECT
@@ -62,6 +123,26 @@ FROM job_postings_fact
 WHERE job_title_short = 'Data Engineer'
    OR job_title_short = 'Data Scientist'
 LIMIT 10;
+/*
+
+┌────────┬─────────────────┬────────────────┐
+│ job_id │ job_title_short │  job_location  │
+│ int32  │     varchar     │    varchar     │
+├────────┼─────────────────┼────────────────┤
+│   4618 │ Data Engineer   │ Austin, TX     │
+│   4638 │ Data Scientist  │ New York, NY   │
+│   4639 │ Data Scientist  │ Anywhere       │
+│   4640 │ Data Scientist  │ Vienna, VA     │
+│   4641 │ Data Scientist  │ Washington, DC │
+│   4642 │ Data Scientist  │ Edison, NJ     │
+│   4643 │ Data Scientist  │ Raritan, NJ    │
+│   4644 │ Data Scientist  │ Anywhere       │
+│   4646 │ Data Scientist  │ Washington, DC │
+│   4647 │ Data Scientist  │ Cambridge, MA  │
+├────────┴─────────────────┴────────────────┤
+│ 10 rows                         3 columns │
+└───────────────────────────────────────────┘
+*/
 
 -- Combining AND + OR (use parentheses to be clear!)
 SELECT
